b7db13a765c1f45fe3b8f69741cb1b4d03cbbfb9
amcollspain <acmgedmath@gmail.com>
2026-03-24 00:40:11 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// ===== EDIT THIS ONLY =====
+
+const SCHOOL_NAME = "American College Spain";
+const SESSION_NAME = "Spring 2026 Session";
+const SESSION_DATE = "23 March 2026";
+
+// Change this each exam
+const INVIGILATOR_PIN = "1234";
+
+// Change color if desired
+const BANNER_COLOR = "#00274c";
+
+// ==========================
