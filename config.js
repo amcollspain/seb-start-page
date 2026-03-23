7057dde47b67c07e7b7bb9c6c30160714715f39c
amcollspain <acmgedmath@gmail.com>
2026-03-24 00:42:44 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ const SCHOOL_NAME = "American College Spain";
 const SESSION_NAME = "Spring 2026 Session";
 const SESSION_DATE = "23 March 2026";
 
-// Change this each exam
-const INVIGILATOR_PIN = "1234";
+// Hashed PIN for "1234"
+const PIN_HASH = "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4";
 
-// Change color if desired
+// Banner color
 const BANNER_COLOR = "#00274c";
 
 // ==========================
